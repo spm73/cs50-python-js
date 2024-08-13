cdc5ce4a755be7254dd597a0309dec733a6a4e58
spm73 <sergiopascual05@gmail.com>
2024-08-13 21:17:01 +0200

    new, random, links and part of search done

--- project1/wiki/encyclopedia/urls.py
+++ project1/wiki/encyclopedia/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'encyclopedia'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry")
+    path('new', views.new, name="new"),
+    path('random', views.random_page, name="random_page"),
+    path('search', views.search, name="search"),
+    path("<str:title>", views.entry, name="entry"),
 ]
--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponseNotFound, HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponseNotFound, HttpResponse #, HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 from markdown2 import Markdown
+from random import choice
 
 
-def index(request):
+def index(request) -> HttpResponse:
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
@@ -17,3 +19,26 @@ def entry(request, title: str) -> HttpResponse:
         return HttpResponseNotFound(f"There is no entry for {title}")
     
     return HttpResponse(Markdown().convert(content))
+
+
+def new(request) -> HttpResponse:
+    return HttpResponse("Create a new page")
+
+
+def random_page(request) -> HttpResponse:
+    # return HttpResponseRedirect(reverse("encyclopedia:entry", args=[choice(util.list_entries())]))
+    return redirect(reverse("encyclopedia:entry", args=[choice(util.list_entries())]))
+
+
+def search(request) -> HttpResponse:
+    if request.method != "POST":
+        return redirect(reverse("encyclopedia:index"))
+        
+    entry_title = request.POST["q"]
+    if entry_title in util.list_entries():
+        return redirect(reverse("encyclopedia:entry", args=[entry_title]))
+    else:
+        return render(request, 'encyclopedia/search_error.html', {
+            "entries": util.list_entries(),
+            "entry_title": entry_title
+        })
